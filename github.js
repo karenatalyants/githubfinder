be58f8d2fc705077dcc05a28c0c16a8dd818bbca
Karen Atalyants <karenatalyants@gmail.com>
2019-03-28 19:07:26 +0400

    simple github search

---
+++ github.js
@@ -0,0 +1,21 @@
+class GitHub {
+  constructor() {
+    this.client_id = "fb64309b40963d3a3c16",
+      this.client_secret = "cb07c16fd8acba289b4b090f874b9ac71cde3f60",
+      this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
